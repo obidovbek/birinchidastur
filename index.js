651bb46864898fef50091aa61eac2c3e3fdfebfa
obidovbek <obidov.bekzod@gmail.com>
2024-11-08 15:33:08 +0500

    6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Salom 2");
+  res.send("Salom 3");
 });
 app.listen(4300, () => {
   console.log(`Server 4300 portida ishladi`);
